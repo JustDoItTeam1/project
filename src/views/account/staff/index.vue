b234f2b8ec650b414b796cab00d956cc793a544a
XunLi0029 <lixun8070@163.com>
2020-12-05 22:32:07 +0800

    账户管理：施工单位、交管人员、管理员账户管理。 施工项目管理。

--- src/views/account/staff/index.vue
+++ src/views/account/staff/index.vue
@@ -10,15 +10,15 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="密码" prop="trafficPassword">
-        <el-input
-          v-model="queryParams.trafficPassword"
-          placeholder="请输入密码"
-          clearable
-          size="small"
-          @keyup.enter.native="handleQuery"
-        />
-      </el-form-item>
+      <!--      <el-form-item label="密码" prop="trafficPassword">-->
+      <!--        <el-input-->
+      <!--          v-model="queryParams.trafficPassword"-->
+      <!--          placeholder="请输入密码"-->
+      <!--          clearable-->
+      <!--          size="small"-->
+      <!--          @keyup.enter.native="handleQuery"-->
+      <!--        />-->
+      <!--      </el-form-item>-->
       <el-form-item>
         <el-button type="cyan" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
         <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
@@ -64,34 +64,34 @@
           v-hasPermi="['account:staff:export']"
         >导出</el-button>
       </el-col>
-	  <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="staffList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="交管人员id" align="center" prop="trafficId" />
       <el-table-column label="用户账户" align="center" prop="trafficName" />
-      <el-table-column label="密码" align="center" prop="trafficPassword" />
+      <!--      <el-table-column label="密码" align="center" prop="trafficPassword" />-->
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
             size="mini"
             type="text"
             icon="el-icon-edit"
-            @click="handleUpdate(scope.row)"
+            @click="handleUpdate"
             v-hasPermi="['account:staff:edit']"
           >修改</el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
-            @click="handleDelete(scope.row)"
+            @click="handleDelete"
             v-hasPermi="['account:staff:remove']"
           >删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
       v-show="total>0"
       :total="total"
@@ -119,132 +119,132 @@
 </template>
 
 <script>
-import { listStaff, getStaff, delStaff, addStaff, updateStaff, exportStaff } from "@/api/account/staff";
+  import { listStaff, getStaff, delStaff, addStaff, updateStaff, exportStaff } from "@/api/account/staff";
 
-export default {
-  name: "Staff",
-  data() {
-    return {
-      // 遮罩层
-      loading: true,
-      // 选中数组
-      ids: [],
-      // 非单个禁用
-      single: true,
-      // 非多个禁用
-      multiple: true,
-      // 显示搜索条件
-      showSearch: true,
-      // 总条数
-      total: 0,
-      // 交管人员用户表格数据
-      staffList: [],
-      // 弹出层标题
-      title: "",
-      // 是否显示弹出层
-      open: false,
-      // 查询参数
-      queryParams: {
-        pageNum: 1,
-        pageSize: 10,
-        trafficName: null,
-        trafficPassword: null,
-      },
-      // 表单参数
-      form: {},
-      // 表单校验
-      rules: {
-      }
-    };
-  },
-  created() {
-    this.getList();
-  },
-  methods: {
-    /** 查询交管人员用户列表 */
-    getList() {
-      this.loading = true;
-      listStaff(this.queryParams).then(response => {
-        this.staffList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
-    },
-    // 取消按钮
-    cancel() {
-      this.open = false;
-      this.reset();
-    },
-    // 表单重置
-    reset() {
-      this.form = {
-        trafficId: null,
-        trafficName: null,
-        trafficPassword: null,
-        trafficDeleteFlag: null
+  export default {
+    name: "Staff",
+    data() {
+      return {
+        // 遮罩层
+        loading: true,
+        // 选中数组
+        ids: [],
+        // 非单个禁用
+        single: true,
+        // 非多个禁用
+        multiple: true,
+        // 显示搜索条件
+        showSearch: true,
+        // 总条数
+        total: 0,
+        // 交管人员用户表格数据
+        staffList: [],
+        // 弹出层标题
+        title: "",
+        // 是否显示弹出层
+        open: false,
+        // 查询参数
+        queryParams: {
+          pageNum: 1,
+          pageSize: 10,
+          trafficName: null,
+          trafficPassword: null,
+        },
+        // 表单参数
+        form: {},
+        // 表单校验
+        rules: {
+        }
       };
-      this.resetForm("form");
     },
-    /** 搜索按钮操作 */
-    handleQuery() {
-      this.queryParams.pageNum = 1;
+    created() {
       this.getList();
     },
-    /** 重置按钮操作 */
-    resetQuery() {
-      this.resetForm("queryForm");
-      this.handleQuery();
-    },
-    // 多选框选中数据
-    handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.trafficId)
-      this.single = selection.length!==1
-      this.multiple = !selection.length
-    },
-    /** 新增按钮操作 */
-    handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加交管人员用户";
-    },
-    /** 修改按钮操作 */
-    handleUpdate(row) {
-      this.reset();
-      const trafficId = row.trafficId || this.ids
-      getStaff(trafficId).then(response => {
-        this.form = response.data;
+    methods: {
+      /** 查询交管人员用户列表 */
+      getList() {
+        this.loading = true;
+        listStaff(this.queryParams).then(response => {
+          this.staffList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        });
+      },
+      // 取消按钮
+      cancel() {
+        this.open = false;
+        this.reset();
+      },
+      // 表单重置
+      reset() {
+        this.form = {
+          trafficId: null,
+          trafficName: null,
+          trafficPassword: null,
+          trafficDeleteFlag: null,
+        };
+        this.resetForm("form");
+      },
+      /** 搜索按钮操作 */
+      handleQuery() {
+        this.queryParams.pageNum = 1;
+        this.getList();
+      },
+      /** 重置按钮操作 */
+      resetQuery() {
+        this.resetForm("queryForm");
+        this.handleQuery();
+      },
+      // 多选框选中数据
+      handleSelectionChange(selection) {
+        this.ids = selection.map(item => item.trafficId)
+        this.single = selection.length!==1
+        this.multiple = !selection.length
+      },
+      /** 新增按钮操作 */
+      handleAdd() {
+        this.reset();
         this.open = true;
-        this.title = "修改交管人员用户";
-      });
-    },
-    /** 提交按钮 */
-    submitForm() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.trafficId != null) {
-            updateStaff(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("修改成功");
-                this.open = false;
-                this.getList();
-              }
-            });
-          } else {
-            addStaff(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("新增成功");
-                this.open = false;
-                this.getList();
-              }
-            });
+        this.title = "添加交管人员用户";
+      },
+      /** 修改按钮操作 */
+      handleUpdate(row) {
+        this.reset();
+        const trafficId = row.trafficId || this.ids
+        getStaff(trafficId).then(response => {
+          this.form = response.data;
+          this.open = true;
+          this.title = "修改交管人员用户";
+        });
+      },
+      /** 提交按钮 */
+      submitForm() {
+        this.$refs["form"].validate(valid => {
+          if (valid) {
+            if (this.form.trafficId != null) {
+              updateStaff(this.form).then(response => {
+                if (response.code === 200) {
+                  this.msgSuccess("修改成功");
+                  this.open = false;
+                  this.getList();
+                }
+              });
+            } else {
+              addStaff(this.form).then(response => {
+                if (response.code === 200) {
+                  this.msgSuccess("新增成功");
+                  this.open = false;
+                  this.getList();
+                }
+              });
+            }
           }
-        }
-      });
-    },
-    /** 删除按钮操作 */
-    handleDelete(row) {
-      const trafficIds = row.trafficId || this.ids;
-      this.$confirm('是否确认删除交管人员用户编号为"' + trafficIds + '"的数据项?', "警告", {
+        });
+      },
+      /** 删除按钮操作 */
+      handleDelete(row) {
+        const trafficIds = row.trafficId || this.ids;
+        this.$confirm('是否确认删除交管人员用户编号为"' + trafficIds + '"的数据项?', "警告", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
           type: "warning"
@@ -254,11 +254,11 @@ export default {
           this.getList();
           this.msgSuccess("删除成功");
         }).catch(function() {});
-    },
-    /** 导出按钮操作 */
-    handleExport() {
-      const queryParams = this.queryParams;
-      this.$confirm('是否确认导出所有交管人员用户数据项?', "警告", {
+      },
+      /** 导出按钮操作 */
+      handleExport() {
+        const queryParams = this.queryParams;
+        this.$confirm('是否确认导出所有交管人员用户数据项?', "警告", {
           confirmButtonText: "确定",
           cancelButtonText: "取消",
           type: "warning"
@@ -267,7 +267,7 @@ export default {
         }).then(response => {
           this.download(response.msg);
         }).catch(function() {});
+      }
     }
-  }
-};
+  };
 </script>
